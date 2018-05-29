a3f324ada671cd738a324fca600286bf3d19e881
zhangweilin <willzhang003@gmail.com>
2018-05-29 23:00:54 +0800

    some revise

--- Python/data_structure/binary_tree.py
+++ Python/data_structure/binary_tree.py
@@ -1,6 +1,5 @@
 
-# 参考博客 http://www.cnblogs.com/jiuyang/p/7928248.html
-# https://www.cnblogs.com/freeman818/p/7252041.html
+# 参考博客  https://www.cnblogs.com/freeman818/p/7252041.html
 # 实现二叉树遍历
 
 class Node:
@@ -37,27 +36,26 @@ def findTree(preList, midList, afterList):# 已知前、中序遍历，求后序
     if len(preList) == 1:
         afterList.append(preList[0])
         return
-    root = preList[0]
-    n = midList.index(root)
-    findTree(preList[1:n + 1], midList[:n], afterList)
-    findTree(preList[n + 1:], midList[n + 1:], afterList)
-    afterList.append(root)
+    root = preList[0]  #先序的第一个节点即根节点
+    n = midList.index(root) #找到根节点在中序中对应的index
+    findTree(preList[1:n + 1], midList[:n], afterList) #对左子树采取相同的操作
+    findTree(preList[n + 1:], midList[n + 1:], afterList) #对右子树采取相同操作
+    afterList.append(root)  #只有到叶子节点时才加到afterList中，顺序也是先左后右，最后才是往上递归到根节点
     return afterList
 
 
 if __name__ == '__main__':
-    if __name__ == '__main__':
-        root = Node('D', Node('B', Node('A'), Node('C')), Node('E', right=Node('G', Node('F'))))
-        print('前序遍历：')
-        preTraverse(root)
-        print('中序遍历：')
-        midTraverse(root)
-        print('后序遍历：')
-        afterTraverse(root)
+    root = Node('D', Node('B', Node('A'), Node('C')), Node('E', right=Node('G', Node('F'))))
+    print('前序遍历：')
+    preTraverse(root)
+    print('中序遍历：')
+    midTraverse(root)
+    print('后序遍历：')
+    afterTraverse(root)
 
-        print('已知前，中序遍历求后续遍历')
-        preList = list('DBACEGF')
-        midList = list('ABCDEFG')
-        afterList = []
-        after = findTree(preList,midList,afterList)
-        print(after)
+    print('已知前，中序遍历求后续遍历')
+    preList = list('DBACEGF')
+    midList = list('ABCDEFG')
+    afterList = []
+    after = findTree(preList,midList,afterList)
+    print(after)
